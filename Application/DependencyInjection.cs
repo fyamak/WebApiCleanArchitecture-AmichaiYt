c47152e454a96d44341fdd24476fb3bc4bde4990
fyamak <furkanyamaak@gmail.com>
2025-04-04 10:29:59 +0300

    part1 - project setup

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Services.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddAplication(this IServiceCollection services)
+    {
+        services.AddScoped<IAuthenticationService, AuthenticationService>();
+
+
+        return services;
+    }
+}
