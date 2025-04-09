56ba84a63c2037d60b10e49240de677a8b7084a0
fyamak <furkanyamaak@gmail.com>
2025-04-09 14:11:13 +0300

    part9 - Jwt Bearer Authentication

---
+++ Api/Controllers/DinnerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class DinnerController : ControllerBase
+{
+    [HttpGet]
+    [Authorize]
+    public IActionResult ListDinners()
+    {
+        return Ok("List");
+    }
+}
